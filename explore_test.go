c87abe616cc13148b25cb558ec0123e7523a1196
Matthew Rothenberg <mrothenberg@gmail.com>
2023-08-09 08:42:11 +0100

    test: loopclosure pin

--- explore_test.go
+++ explore_test.go
@@ -105,6 +105,7 @@ func TestExplorer(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
+		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel() // run in parallel to validate thread safety
 
